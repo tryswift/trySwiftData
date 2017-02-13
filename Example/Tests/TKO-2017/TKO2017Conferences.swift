f4d131ad77290c4592543f99524a2f91acf703f3
Tim Oliver <me@timoliver.com.au>
2017-02-12 16:22:17 -0800

    Updated conference descriptions

--- Example/Tests/TKO-2017/TKO2017Conferences.swift
+++ Example/Tests/TKO-2017/TKO2017Conferences.swift
@@ -14,10 +14,10 @@ public let tko2017Conferences: [Conference] = [
     {
         let trySwift = Conference()
         trySwift.name = "try! Conference"
-        trySwift.twitter = "tryswiftnyc"
+        trySwift.twitter = "tryswiftconf"
         trySwift.imageName = "logo"
         trySwift.imageURL = nil
-        trySwift.conferenceDescription = "try! Conference is an immersive community gathering about Swift Language Best Practices, Application Development in Swift, Server-Side Swift, Open Source Swift, and the Swift Community, taking place in New York City on September 1st and 2nd, 2016."
+        trySwift.conferenceDescription = "try! Conference is an immersive community gathering about Swift Language Best Practices, Application Development in Swift, Server-Side Swift, Open Source Swift, and the Swift Community, taking place in Tokyo on March 2nd through 4th, 2017."
         trySwift.venue = tko2017Venues["bellesalle-shinjuku"]
         trySwift.organizer = nyc2016Speakers["natashatherobot"]
         return trySwift
