cabfd4ce182153863ad8fa18dc9cdc9cefc5e243
NatashaTheRobot <natasha@natashatherobot.com>
2017-03-26 16:29:40 +0900

    added talk session tests

---
+++ Example/Tests/SessionTests/TalkSessionViewModelTests.swift
@@ -0,0 +1,57 @@
+//
+//  TalkSessionViewModelTests.swift
+//  trySwiftData
+//
+//  Created by Natasha Murashev on 3/26/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import TrySwiftData
+
+class TalkSessionViewModelTests: XCTestCase {
+    
+    fileprivate let conference = tko2017Conferences.first!
+    
+    fileprivate let talk = tko2017Sessions["day1Alexis"]!
+    
+    fileprivate var viewModel: SessionViewModel!
+    fileprivate var presentation: Presentation!
+    
+    override func setUp() {
+        viewModel = SessionViewModel(session: talk)
+        presentation = talk.presentation!
+    }
+    
+    func testTitle() {
+        XCTAssertEqual( viewModel.title, presentation.localizedTitle)
+    }
+    
+    func testSubtitle() {
+        XCTAssertEqual(viewModel.subtitle, presentation.speaker!.localizedName)
+    }
+    
+    func testLogoURL() {
+        XCTAssertEqual(viewModel.logoURL.lastPathComponent, presentation.speaker!.imageAssetName)
+    }
+    
+    func testLocation() {
+        XCTAssertEqual(viewModel.location, talk.location!.name)
+    }
+    
+    func testSessionDescription() {
+        XCTAssertEqual(viewModel.sessionDescription, "Presentation")
+    }
+    
+    func testPresentationSummary() {
+        XCTAssertEqual(viewModel.presentationSummary, presentation!.localizedSummary)
+    }
+    
+    func testSelectable() {
+        XCTAssertTrue(viewModel.selectable)
+    }
+    
+    func testTwitter() {
+        XCTAssertEqual(viewModel.twitter, "@\(presentation.speaker!.twitter)")
+    }
+}
