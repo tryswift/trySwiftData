c1d57703c0dedcf1dd63498af7d2f6da7abd1f83
Alvin Varghese <alvinvarghese@live.com>
2017-09-22 11:34:11 +0700

    Created Group for Bangalore 2017, and added necessary files into the Group

---
+++ Example/Tests/BLR-2017/BLR2017ConferenceDays.swift
@@ -0,0 +1,32 @@
+//
+//  NYC2016ConferenceDays.swift
+//  TrySwiftData
+//
+//  Created by Tim Oliver on 1/29/17.
+//  Copyright © 2017 NatashaTheRobot. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+import RealmSwift
+import TrySwiftData
+
+public let nyc2017ConferenceDays: [ConferenceDay] = [
+    {
+        let day1 = ConferenceDay()
+        day1.date = Date.date(year: 2017, month: 9, day: 4, hour: 14, minute: 0, second: 0)
+        for index in 0...18 {
+            day1.sessionBlocks.append(nyc2017SessionBlocks[index])
+        }
+        return day1
+    }(),
+    {
+        let day2 = ConferenceDay()
+        day2.date = Date.date(year: 2017, month: 9, day: 5, hour: 8, minute: 0, second: 0)
+        for index in 19...35 {
+            day2.sessionBlocks.append(nyc2017SessionBlocks[index])
+        }
+        return day2
+    }()
+]
