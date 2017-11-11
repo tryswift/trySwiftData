95b3fdada6119ca8fb9d2475425947637d1e57ad
NatashaTheRobot <natasha@natashatherobot.com>
2017-11-11 16:42:15 +0700

    small refactoring of adding organizers and venues

--- Example/Tests/BLR-2017/BLR2017Conferences.swift
+++ Example/Tests/BLR-2017/BLR2017Conferences.swift
@@ -19,13 +19,10 @@ public let blr2017Conferences: [Conference] = [
         trySwift.twitter = "tryswiftindia"
         trySwift.logoAssetName = "Logo_BLR.png"
         trySwift.conferenceDescription = "try! Swift is the biggest international iOS developer conference in the world focusing on the new Swift Programming Language. try! Swift India is an amazing chance for developers in the Asian Pacific region to learn the latest world trends in iOS development using the industry's best standards. Learn from 15 international speakers, engage with the community. Coming Bangalore on November 19th 2017!"
-        trySwift.organizers.append(blr2017Organizers["natashatherobot"]!)
-        trySwift.organizers.append(blr2017Organizers["alvin"]!)
-        trySwift.organizers.append(blr2017Organizers["vaish"]!)
-        trySwift.organizers.append(blr2017Organizers["giridhar"]!)
-        trySwift.venues.append(blr2017Venues["thegrandmagrathhotel"]!) // Conference Venue
-        trySwift.venues.append(blr2017Venues["swiftrobotworkshop"]!)
-        trySwift.venues.append(blr2017Venues["swiftguruworkshop"]!)
+        
+        blr2017Venues.forEach { trySwift.venues.append($1) }
+        blr2017Organizers.forEach { trySwift.organizers.append($1) }
+        
         return trySwift
     }()
 ]
