961c5a9ce76e60cc4d4a62bb9591fd7a7626be9e
NatashaTheRobot <natasha@natashatherobot.com>
2018-02-17 19:27:17 +0900

    added extra properties to conference data

--- Example/Tests/TKO-2018/TKO2018Conferences.swift
+++ Example/Tests/TKO-2018/TKO2018Conferences.swift
@@ -17,6 +17,8 @@ public let tko2018Conferences: [Conference] = [
     let trySwift = Conference()
     trySwift.name = "try! Conference"
     trySwift.twitter = "tryswiftconf"
+    trySwift.email = "tokyo@tryswift.co"
+    trySwift.slackURL = "https://tryswifttokyo2018.slack.com"
     trySwift.logoAssetName = "Logo_TKO.png"
     trySwift.conferenceDescription = "try! Swift is the biggest international iOS developer conference in the world focusing on the new Swift Programming Language. try! Swift Tokyo is an amazing chance for developers from around the world to learn the latest world trends in iOS development using the industry's best standards. Learn from 21 international speakers, engage with the community. Coming to Tokyo on March 1st and 2nd, with Peer Labs and Workshops on March 3rd."
     
