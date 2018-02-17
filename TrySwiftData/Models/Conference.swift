d97dbe1ebb0236b5a197e90ec8ecfe68ef97b7f1
NatashaTheRobot <natasha@natashatherobot.com>
2018-02-17 19:37:45 +0900

    updated slack url

--- TrySwiftData/Models/Conference.swift
+++ TrySwiftData/Models/Conference.swift
@@ -16,7 +16,7 @@ public class Conference: Object {
     @objc open dynamic var conferenceDescription: String = ""
     @objc open dynamic var conferenceDescriptionJP: String?
     @objc open dynamic var email: String = "info@tryswift.co"
-    @objc open dynamic var slackURL: String?
+    @objc open dynamic var slackURL: String = ""
     @objc open dynamic var codeOfConductURL: String = "https://www.tryswift.co/code-of-conduct/"
     open let venues = List<Venue>()
     open let organizers = List<Organizer>()
